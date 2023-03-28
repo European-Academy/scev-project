09790251d088e70efbf0505f39ae279433c6673b
asykinkaragiorgis <asykin.karagiorgis@outlook.com>
2023-03-28 10:57:16 +0300

    icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,33 @@
 import '../styles/globals.css'
 import '../styles/custom.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+  <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <meta name="theme-color" content="#440099" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#440099" />
+      </Head>
+    <Component {...pageProps} />
+  </>
+  )
 }
