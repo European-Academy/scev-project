5f254f196f72888397643b36f522190bb36bf159
asykinkaragiorgis <asykin.karagiorgis@outlook.com>
2023-03-28 10:00:11 +0300

    nav mobile view

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -18,7 +18,7 @@ export const NavBar: React.FC<{
     const Btns = ({ data, dataLink }: any) => {
         return (
             <>
-                <div>
+                <div className="py-2 md:py-0">
                     <Link to={dataLink} spy={true} smooth={true}>
                         <button className="text-black hover:text-gray font-poppinsBold text-lg uppercase">{data}</button>
                     </Link>
@@ -46,7 +46,7 @@ export const NavBar: React.FC<{
                                 </Links>
                             </div>
                         </div>
-                        <div className="lg:flex lg:gap-x-12">
+                        <div className="hidden md:block lg:flex lg:gap-x-12">
                             <Btns data="Home" dataLink="to_banner" />
                             <Btns data="Why Us" dataLink="to_whyus" />
                             <Btns data="Our Services" dataLink="to_services" />
@@ -73,9 +73,9 @@ export const NavBar: React.FC<{
                 >
                     <Popover.Panel
                         focus
-                        className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                        className="absolute inset-x-0 p-2 transition transform origin-top-right md:hidden"
                     >
-                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50 border-2">
                             <div className="pt-5 pb-6 px-5">
                                 <div className="flex items-center justify-end">
                                     {/* <div className="h-16 w-auto">
@@ -91,11 +91,20 @@ export const NavBar: React.FC<{
                                         </Link>
                                     </div> */}
                                     <div className="-mr-2">
-                                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:bg-gray-100 focus:outline-none ">
                                             <span className="sr-only">Close menu</span>
                                             <XIcon className="h-6 w-6" aria-hidden="true" />
                                         </Popover.Button>
+
                                     </div>
+
+                                </div>
+                                <div className="py-4">
+                                    <Btns data="Home" dataLink="to_banner" />
+                                    <Btns data="Why Us" dataLink="to_whyus" />
+                                    <Btns data="Our Services" dataLink="to_services" />
+                                    <Btns data="About Us" dataLink="to_about" />
+                                    <Btns data="Contact" dataLink="to_contact" />
                                 </div>
                             </div>
                         </div>
